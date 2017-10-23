1d3457956151609d0d8eeb6e9890f02bf2cb7308
Николай Эмрих <redishko@gmail.com>
2017-10-24 01:48:28 +0300

    Добавляет обработчик хэшей

--- js/app.js
+++ js/app.js
@@ -2,16 +2,65 @@ import greetingScreen from './greeting-screen/greeting';
 import gameScreen from './game-screen/game';
 import resultScreen from './result-screen/result';
 
+// #game?{"answers": [{"timeInSec": 20, "isCorrectAnswer": false}]}
+//
+// #result?{"status": "time-over"}
+// #result?{"status": "attempts-over"}
+// #result?{"status": "win", "score"; 17, "winInSeconds": 68, "fastAnswersCount": 7, "mistakesCnt": 2}
+
 export default class App {
+  static init() {
+    const hashChangeHandler = () => {
+      if (!location.hash) {
+        this.showGreeting();
+        return;
+      }
+      const hashValue = location.hash.replace(`#`, ``);
+      const [hash, json] = hashValue.split(`?`);
+      try {
+        const obj = JSON.parse(json);
+        switch (hash) {
+          case `game`:
+            const answers = obj.answers;
+            if (!(answers instanceof Array)) {
+              throw new TypeError(`No answers or wrong type`);
+            }
+            const correctArray = answers.every((ans) => {
+              return ans.hasOwnProperty(`isCorrectAnswer`) && ans.hasOwnProperty(`timeInSec`);
+            });
+            if (!correctArray) {
+              throw new Error(`Array has wrong data`);
+            }
+            this.showGame(answers);
+            break;
+          case `result`:
+            this.showResult(obj);
+            break;
+        }
+      } catch (err) {
+        switch (hash) {
+          case `game`:
+            this.showGame();
+            break;
+          case `result`:
+            this.showGreeting();
+            break;
+        }
+      }
+    };
+    window.onhashchange = hashChangeHandler;
+    hashChangeHandler();
+  }
+
   static showGreeting() {
     greetingScreen.init();
   }
 
-  static showGame() {
-    gameScreen.init();
+  static showGame(answers) {
+    gameScreen.init(answers);
   }
 
-  static showResult(state) {
-    resultScreen.init(state);
+  static showResult(gameResult) {
+    resultScreen.init(gameResult);
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
 import App from './app';
 
-
-App.showGreeting();
-
-// App.showGame({
-//   answers: new Array(10).fill({
-//     timeInSec: 10,
-//     isCorrectAnswer: false
-//   })
-// });
-
-// App.showResult({
-//   status: `win`,
-//   score: 12,
-//   winInSeconds: 30,
-//   fastAnswersCount: 7,
-//   mistakesCnt: 2
-// });
-
-// App.showResult({
-//   status: `time-over`
-// });
-// App.showResult({
-//   status: `attempts-over`
-// });
+App.init();
