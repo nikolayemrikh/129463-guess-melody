ee88dd311c298ddca2e279572ebfd603c3ce4651
Николай Эмрих <redishko@gmail.com>
2017-11-04 16:31:13 +0300

    Убирает шифрование, добавляет обработку неверных хэшей

--- js/app.js
+++ js/app.js
@@ -2,27 +2,6 @@ import greetingScreen from './greeting-screen/greeting';
 import GameScreen from './game-screen/game';
 import resultScreen from './result-screen/result';
 import config from './config';
-// #game?{"answers": [{"timeInSec": 20, "isCorrect": false}]}
-// #result?{"status": "time-over"}
-// #result?{"status": "attempts-over"}
-// #result?{"status": "win", "score"; 17, "winInSeconds": 68, "fastAnswersCount": 7, "mistakesCnt": 2}
-// https://github.com/diafygi/webcrypto-examples#rsa-oaep---encrypt
-
-const cryptoKeys = {};
-const importKey = (key, isPrivate) => {
-  return new Promise((resolve, reject) => {
-    window.crypto.subtle.importKey(
-        `jwk`,
-        key,
-        {
-          name: `RSA-OAEP`,
-          hash: {name: `SHA-1`},
-        },
-        false,
-        [isPrivate ? `decrypt` : `encrypt`]
-    ).then(resolve).catch(reject);
-  });
-};
 
 /**
  * Перечисление идентификаторов экранов
@@ -34,108 +13,21 @@ const ScreenHash = {
   RESULT: `result`
 };
 
-/**
-  * @param {Object} gameResult — словарь состояний игры
-  * @return {string} — строка, содержащая объединенные символом ":" числа,
-  * описывающие состояние игры
-*/
-const joinGameResultValues = (gameResult) => {
-  const {status, score, winInSeconds, fastAnswersCount, mistakesCnt} = gameResult;
-  const arr = [status, score, winInSeconds, fastAnswersCount, mistakesCnt];
-  return arr.join(`:`);
-};
-
-/**
-  * Ф-ия, обратная ф-ии joinGameResultValues
-  * @param {Object} strOfHexes — строка, содержащая объединенные символом ":" числа,
-  * описывающие состояние игры
-  * @return {Object} — словарь состояний игры
-*/
-const getGameResultFromJoinedHexes = (strOfHexes) => {
-  const [status, score, winInSeconds, fastAnswersCount, mistakesCnt] = strOfHexes
-      .split(`:`).map((str) => Number.parseInt(str, 10));
-  return {
-    status,
-    score,
-    winInSeconds,
-    fastAnswersCount,
-    mistakesCnt
-  };
-};
-
-const textEncoder = new TextEncoder();
-const textDecoder = new TextDecoder();
-
-/**
- * @async
- * @param {Object} privateKey — приватный ключ из связки
- * @param {string} strOfHexes — строка, содержащая объедененные символом ":"
- * hex-ы, в которые превращены десятичные элементы буффера Uint8Array
- * @return {Promise.<string>} — расшифрованная и раскодированная строка состояний игры
-*/
-const decryptResult = (privateKey, strOfHexes) => {
-  if (typeof strOfHexes !== `string`) {
-    throw new TypeError(`arguments[1] is not of type string`);
-  }
-  const arr = strOfHexes.split(`:`).map((str) => Number.parseInt(str, 16));
-  const encryptedBuffer = new Uint8Array(arr);
-  return new Promise((resolve, reject) => {
-    window.crypto.subtle.decrypt({
-      name: `RSA-OAEP`
-    },
-    privateKey,
-    encryptedBuffer).then((decrypted) => {
-      const decryptedBuffer = new Uint8Array(decrypted);
-      const decoded = textDecoder.decode(decryptedBuffer);
-      const gameResult = getGameResultFromJoinedHexes(decoded);
-      resolve(gameResult);
-    }).catch(reject);
-  });
-};
-
-/**
- * @async
- * @param {Object} publicKey — публичный ключ из связки
- * @param {string} gameResult — словарь состояний игры
- * @return {Promise.<string>} — закодированная и зашифрованная строка состояний игры
-*/
-const encryptResult = (publicKey, gameResult) => {
-  const str = joinGameResultValues(gameResult);
-  const buffer = textEncoder.encode(str);
-  return new Promise((resolve, reject) => {
-    window.crypto.subtle.encrypt({
-      name: `RSA-OAEP`
-    },
-    publicKey,
-    buffer
-    ).then((encrypted) => {
-      const encryptedResult = new Uint8Array(encrypted);
-      const joinedHexes = [...encryptedResult].map((num) => num.toString(16)).join(`:`);
-      resolve(joinedHexes);
-    }).catch(reject);
-  });
-};
-
 export default class App {
   /**
    * Обрабатывает URL и вызывает метод changeScreen
-   * Импортирует публичный и приватный ключи из связки в конфиге
    */
   static init() {
     const hashChangeHandler = () => {
-      const hashValue = location.hash.replace(`#`, ``);
-      const [hash, data] = hashValue.split(`?`);
-      this.changeScreen(hash, data);
+      const hash = location.hash.replace(`#`, ``);
+      if (hash !== ScreenHash.GREETING && hash !== ScreenHash.GAME) {
+        location.hash = ScreenHash.GREETING;
+      }
+      this.changeScreen(hash);
     };
     window.onhashchange = hashChangeHandler;
-    const keyPromises = [
-      importKey(config.keypair.public, false),
-      importKey(config.keypair.private, true)
-    ];
-    const keyPromiseAll = Promise.all(keyPromises);
-    const allPromises = [keyPromiseAll, fetch(config.dataUrl)];
-    Promise.all(allPromises).then(([keys, questionsData]) => {
-      [cryptoKeys.publicKey, cryptoKeys.privateKey] = keys;
+
+    fetch(config.dataUrl).then((questionsData) => {
       questionsData.json().then((questions) => {
         this.questions = questions;
         this.gameScreen = new GameScreen(questions);
@@ -143,39 +35,18 @@ export default class App {
       }).catch(() => {
         hashChangeHandler();
       });
-    }).catch(() => {
-      hashChangeHandler();
     });
   }
+
   /**
-   * @param {string} hash — значения якоря до символа, отделяющего данные в URL
-   * @param {string} data — строка с данными из URL после отделяющего символа
+   * @param {string} hash
    */
-  static changeScreen(hash, data) {
-    if (hash === ScreenHash.GREETING) {
-      greetingScreen.init();
+  static changeScreen(hash) {
+    if (hash === ScreenHash.GAME && this.questions && this.questions.length) {
+      this.gameScreen.init();
       return;
     }
-    switch (hash) {
-      case ScreenHash.GAME:
-        if (this.questions && this.questions.length) {
-          this.gameScreen.init();
-        } else {
-          this.showGreeting();
-        }
-        break;
-      case ScreenHash.RESULT:
-        try {
-          decryptResult(cryptoKeys.privateKey, data).then((gameResult) => {
-            resultScreen.init(gameResult);
-          }).catch(() => {
-            this.showGreeting();
-          });
-        } catch (err) {
-          this.showGreeting();
-        }
-        break;
-    }
+    greetingScreen.init();
   }
 
   /**
@@ -197,9 +68,8 @@ export default class App {
    * @param {Object} gameResult — словарь состояний игры
    */
   static showResult(gameResult) {
-    encryptResult(cryptoKeys.publicKey, gameResult).then((strOfHexes) => {
-      location.hash = `${ScreenHash.RESULT}?${strOfHexes}`;
-    });
+    resultScreen.init(gameResult);
+    history.pushState(null, null, `#${ScreenHash.RESULT}`);
   }
 
   static postResult(gameResult) {
--- js/config.js
+++ js/config.js
@@ -6,29 +6,5 @@ export default {
   finishingTime: 30,
   maxTracksArtist: 3,
   maxTracksGenre: 4,
-  fastAnswerTime: 30,
-  keypair: {
-    public: {
-      "alg": `RSA-OAEP`,
-      "e": `AQAB`,
-      "ext": true,
-      "key_ops": [`encrypt`],
-      "kty": `RSA`,
-      "n": `v4R9j_oqXbBQ_magXqxoRT5XiPwngvlqG02LJAix4pP6eRRM2B6PEV1oi8eU4uNSzY-RZNRo5FXsX_vmwD4oPdAR8W0VXIz4CSx5ADgQ5sPL9INA2p94RJKHA_U6Qr-p9dx_Rcd1P4IZE26c6gsVCIDg6uPb53tOnd_w4kaSFP8`
-    },
-    private: {
-      "alg": `RSA-OAEP`,
-      "d": `DSB7qr6u4LURGB7YpySSEQzw2wbNfoCx8SUjgFjIOIuEA4CMs9OEYuBQJiSL4_FI36YThSG5_syU4w9VBaA9CcEXlsHsWtsnZgYIbeaWvu0EHEe0EWypL-vN8X1iQ4LMzwdEvRNS5aIi1_aLU19ghjFB-PkFb4S6uCWoRuMta2E`,
-      "dp": `eijWJACbcZyeGt-kIcpZuCO_Jk5XM_ePfZpZzIol8nyBJdutRyRjcJUW6PPU6ojl-smXVE6yoVjIchUspzvrxQ`,
-      "dq": `SmTS1opRLFlXQ9q3ky9A5t0OBy3rQj3PJKp_Lgk8dsQ1N9z0RVrYk4JvGHJvo-aaIGVdKekuM2tuTKhrsiBBUQ`,
-      "e": `AQAB`,
-      "ext": true,
-      "key_ops": [`decrypt`],
-      "kty": `RSA`,
-      "n": `v4R9j_oqXbBQ_magXqxoRT5XiPwngvlqG02LJAix4pP6eRRM2B6PEV1oi8eU4uNSzY-RZNRo5FXsX_vmwD4oPdAR8W0VXIz4CSx5ADgQ5sPL9INA2p94RJKHA_U6Qr-p9dx_Rcd1P4IZE26c6gsVCIDg6uPb53tOnd_w4kaSFP8`,
-      "p": `83nII23w-dGgu9Q1A6W3bZ1OejYizZ5l15-knyojcbOjR04krPlHJpK0U3siEmNAoc8H2M71jWTluSJa3-HA8w`,
-      "q": `yV5-6jBhWDUoh4cpovFeFQpUNT8VsrMVYllHcErimzkCysam6bPEGHorgfuaOatO81lW55RSsHbG8pReK9t-xQ`,
-      "qi": `d6_cmS2c-_JCSidMyZRMQp8sxCyb2OJ37Wt7jWTWZ4aw9I--tyi0WKagvEOORhG1_bss-rtjtC6zOKQiJIlHXw`
-    }
-  }
+  fastAnswerTime: 30
 };
